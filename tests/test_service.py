254302bab4891a02f1b1859304a76841ff3e5f8f
sasano8 <y-sasahara@ys-method.com>
2025-09-12 02:00:39 +0900

    update

--- tests/test_service.py
+++ tests/test_service.py
@@ -4,19 +4,20 @@ import pytest
 
 @pytest.mark.parametrize(
     [
+        "service_name",
         "url",
         "method",
         "verify",
         "expect"
     ],
     [
-        ("ca.platform.localtest.me", "get", False, 400),  # bad request
-        ("auth.platform.localtest.me", "get", False, 200),
-        ("console.platform.localtest.me", "get", False, 200),
-        ("s3.platform.localtest.me", "get", False, 403),  # forbidden
+        ("Step CA", "ca.platform.localtest.me", "get", False, 400),
+        ("Pocket ID Auth", "auth.platform.localtest.me", "get", False, 200),
+        ("MinIO Console", "console.platform.localtest.me", "get", False, 200),
+        ("MinIO S3 API", "s3.platform.localtest.me", "get", False, 403),
     ]
 )
-def test_service(url, method, verify, expect):
+def test_service(service_name, url, method, verify, expect):
     url = "https://" + url
     print(url)
     assert httpx.request(method, url, verify=verify).status_code == expect
